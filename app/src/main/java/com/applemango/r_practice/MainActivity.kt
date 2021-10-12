f7aa3ca21c6e02cb88987f1b6c6b6b84c4918317
Jaehi <damnyomoonwon1003@gmail.com>
2021-10-12 17:24:06 +0900

    MainAdapter Connect

--- app/src/main/java/com/applemango/r_practice/MainActivity.kt
+++ app/src/main/java/com/applemango/r_practice/MainActivity.kt
@@ -2,10 +2,27 @@ package com.applemango.r_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mAdapter : MainAdapter
+    val items = mutableListOf<MainItemData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mrecycler = findViewById<RecyclerView>(R.id.recycler_main)
+
+        mAdapter = MainAdapter(this)
+        mrecycler.adapter = mAdapter
+
+        items.apply {
+            add(MainItemData("d","d","d"))
+            mAdapter.itemlist = items
+            mAdapter.notifyDataSetChanged()
+        }
     }
+
+
 }
